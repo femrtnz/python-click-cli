54ffe24925883012023e8b9d345e5d2dbeb25748
Felipe Amaral <femrtnz@gmail.com>
2020-03-05 20:14:54 +0000

    Adding python stream event file

---
+++ events.py
@@ -0,0 +1,43 @@
+import json
+import sseclient
+import signal
+import requests
+
+
+filename = 'events.json'
+url = 'http://live-test-scores.herokuapp.com/scores'
+
+
+class StreamEvent:
+    def get_events(self, killer):
+        response = requests.get(url, stream=True)
+        client = sseclient.SSEClient(response)
+
+        events = open(filename, 'a+')
+
+        for event in client.events():
+            if killer.kill_now:
+                events.close()
+                break
+            print(json.loads(event.data))
+            json.dump(json.loads(event.data), events)
+            events.write('\n')
+
+
+class GracefulKiller:
+    kill_now = False
+
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self, signum, frame):
+        self.kill_now = True
+
+
+if __name__ == '__main__':
+    killer = GracefulKiller()
+    stream = StreamEvent()
+    stream.get_events(killer)
+
+    print("Events saved on file " + filename)
